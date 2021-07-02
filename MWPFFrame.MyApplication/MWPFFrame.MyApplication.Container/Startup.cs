b872637660c549443b27f38eefd26a8dd41d6caa
panming <panming1108@163.com>
2021-07-02 16:10:19 +0800

    button

--- MWPFFrame.MyApplication/MWPFFrame.MyApplication.Container/Startup.cs
+++ MWPFFrame.MyApplication/MWPFFrame.MyApplication.Container/Startup.cs
@@ -14,20 +14,25 @@ namespace MWPFFrame.MyApplication.Container
     {
         protected override void App_Startup(object sender, StartupEventArgs e)
         {
+            //注册全局异常
             RegisterGlobalException();
 
+            //注册语言
             var zhCnLanguage = new Uri("/MWPFFrame.UIControls;component/Languages/zh-Hans.xaml", UriKind.RelativeOrAbsolute);
             var enUsLanguage = new Uri("/MWPFFrame.UIControls;component/Languages/en-Us.xaml", UriKind.RelativeOrAbsolute);
             LanguageManager.RegisterLanguage("zh-cn", new Uri[] { zhCnLanguage });
             LanguageManager.RegisterLanguage("en-Us", new Uri[] { enUsLanguage });
 
+            //注册风格
             var darkBlackTheme = new Uri("/MWPFFrame.UIControls;component/Themes/DarkBlackTheme.xaml", UriKind.RelativeOrAbsolute);
             var lightBlueTheme = new Uri("/MWPFFrame.UIControls;component/Themes/LightBlueTheme.xaml", UriKind.RelativeOrAbsolute);
             ThemeManager.RegisterTheme("DarkBlackTheme", new Uri[] { darkBlackTheme });
             ThemeManager.RegisterTheme("LightBlueTheme", new Uri[] { lightBlueTheme });
 
+            //加载样式
+            var commonResource = new Uri("/MWPFFrame.UIControls;component/Generics/Style.xaml", UriKind.RelativeOrAbsolute);
             var sysResource = new Uri("/MWPFFrame.MyApplication.CustomControls;component/Generic/Style.xaml", UriKind.RelativeOrAbsolute);
-            LoadResources("LightBlueTheme", "zh-cn", new Uri[] { sysResource });
+            LoadResources("LightBlueTheme", "zh-cn", new Uri[] { sysResource, commonResource });
 
             MainWindow window = new MainWindow();
             window.ShowDialog();
---
+++ MWPFFrame.MyApplication/MWPFFrame.UIControls/AttachProperties/CommonAttachProperties.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MWPFFrame.UIControls.AttachProperties
+{
+    public class CommonAttachProperties
+    {
+        #region 圆角
+        public static CornerRadius GetCornerRadius(DependencyObject obj)
+        {
+            return (CornerRadius)obj.GetValue(CornerRadiusProperty);
+        }
+
+        public static void SetCornerRadius(DependencyObject obj, CornerRadius value)
+        {
+            obj.SetValue(CornerRadiusProperty, value);
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.RegisterAttached("CornerRadius", typeof(CornerRadius), typeof(CommonAttachProperties));
+        #endregion
+
+        #region 悬浮背景颜色
+        public static Brush GetMouseOverBackground(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(MouseOverBackgroundProperty);
+        }
+
+        public static void SetMouseOverBackground(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(MouseOverBackgroundProperty, value);
+        }
+
+        public static readonly DependencyProperty MouseOverBackgroundProperty =
+            DependencyProperty.RegisterAttached("MouseOverBackground", typeof(Brush), typeof(CommonAttachProperties));
+        #endregion
+
+        #region 悬浮前景颜色
+        public static Brush GetMouseOverForeground(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(MouseOverForegroundProperty);
+        }
+
+        public static void SetMouseOverForeground(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(MouseOverForegroundProperty, value);
+        }
+
+        public static readonly DependencyProperty MouseOverForegroundProperty =
+            DependencyProperty.RegisterAttached("MouseOverForeground", typeof(Brush), typeof(CommonAttachProperties));
+        #endregion
+
+        #region 悬浮边框颜色
+        public static Brush GetMouseOverBorderBrush(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(MouseOverBorderBrushProperty);
+        }
+
+        public static void SetMouseOverBorderBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(MouseOverBorderBrushProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MouseOverBorderBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MouseOverBorderBrushProperty =
+            DependencyProperty.RegisterAttached("MouseOverBorderBrush", typeof(Brush), typeof(CommonAttachProperties));
+        #endregion
+
+        #region 选中背景颜色
+        public static Brush GetSelectedBackground(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(SelectedBackgroundProperty);
+        }
+
+        public static void SetSelectedBackground(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(SelectedBackgroundProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedBackgroundProperty =
+            DependencyProperty.RegisterAttached("SelectedBackground", typeof(Brush), typeof(CommonAttachProperties));
+        #endregion
+
+        #region 选中前景颜色
+        public static Brush GetSelectedForeground(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(SelectedForegroundProperty);
+        }
+
+        public static void SetSelectedForeground(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(SelectedForegroundProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedForegroundProperty =
+            DependencyProperty.RegisterAttached("SelectedForeground", typeof(Brush), typeof(CommonAttachProperties));
+        #endregion
+
+        #region 选中边框颜色
+        public static Brush GetSelectedBorderBrush(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(SelectedBorderBrushProperty);
+        }
+
+        public static void SetSelectedBorderBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(SelectedBorderBrushProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedBorderBrushProperty =
+            DependencyProperty.RegisterAttached("SelectedBorderBrush", typeof(Brush), typeof(CommonAttachProperties));
+        #endregion
+
+        #region 失效背景颜色
+        public static Brush GetUnEnabledBackground(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(UnEnabledBackgroundProperty);
+        }
+
+        public static void SetUnEnabledBackground(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(UnEnabledBackgroundProperty, value);
+        }
+
+        public static readonly DependencyProperty UnEnabledBackgroundProperty =
+            DependencyProperty.RegisterAttached("UnEnabledBackground", typeof(Brush), typeof(CommonAttachProperties));
+        #endregion
+
+        #region 失效前景颜色
+        public static Brush GetUnEnabledForeground(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(UnEnabledForegroundProperty);
+        }
+
+        public static void SetUnEnabledForeground(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(UnEnabledForegroundProperty, value);
+        }
+
+        public static readonly DependencyProperty UnEnabledForegroundProperty =
+            DependencyProperty.RegisterAttached("UnEnabledForeground", typeof(Brush), typeof(CommonAttachProperties));
+        #endregion
+
+        #region 失效边框颜色
+        public static Brush GetUnEnabledBorderBrush(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(UnEnabledBorderBrushProperty);
+        }
+
+        public static void SetUnEnabledBorderBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(UnEnabledBorderBrushProperty, value);
+        }
+
+        public static readonly DependencyProperty UnEnabledBorderBrushProperty =
+            DependencyProperty.RegisterAttached("UnEnabledBorderBrush", typeof(Brush), typeof(CommonAttachProperties));
+        #endregion
+    }
+}
